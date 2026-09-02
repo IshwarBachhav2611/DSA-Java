dac5c922dd5eed005d9f9fec2c68ec5b71e80627
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-09-02 23:48:10 +0530

    [ADDED]ishwar: Codes related Arrays

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,17 @@
+//Find the largest element in an array
+public class LargestElement {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 45, 18};
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+    }
+}
