f71943bb759b4066fd0a0137cb891e9f69f201b0
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-09-02 23:51:43 +0530

    [ADDED]ishwar: Find Smallest elemet in given Array

---
+++ Arrays/SmallestElement.java
@@ -0,0 +1,17 @@
+//Find Smallest Element
+public class SmallestElement {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 45, 18};
+
+        int smallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest: " + smallest);
+    }
+}
