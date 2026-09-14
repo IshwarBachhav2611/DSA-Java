c55560e72a1752911e154568fb0553fe9925bbea
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-09-14 23:56:37 +0530

    [ADDED]ishwar: Find Sum of All Array Elements

---
+++ Arrays/ArraySum.java
@@ -0,0 +1,15 @@
+//Find Sum of Array Elements
+public class ArraySum {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40};
+
+        int sum = 0;
+
+        for (int num : arr) {
+            sum = sum + num;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
