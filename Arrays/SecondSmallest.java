cfd234045f6ae4616f079c28a446523ed1b45feb
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-09-14 23:53:25 +0530

    [ADDED]ishwar: Find Second Semallest Element in Java

---
+++ Arrays/SecondSmallest.java
@@ -0,0 +1,23 @@
+//Find Second Smallest Element
+public class SecondSmallest {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 45, 18};
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            }
+            else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
+            }
+        }
+
+        System.out.println("Second Smallest: " + secondSmallest);
+    }
+}
